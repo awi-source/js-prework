8f05d5ea37cd03a765e935ddf61274afb6f3ec7a
Agnieszka Witek <witek.aga@gmail.com>
2020-10-10 18:13:59 +0200

    Change functions into anonymous

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 {
-function playGame(playerInput) {
+const playGame = function(playerInput) {
     clearMessages();
-    function getMoveName(argMoveId){
+    const getMoveName = function(argMoveId){
         if(argMoveId === 1){
         return 'kamień';
         } else if(argMoveId === 2){
@@ -16,7 +16,7 @@ function playGame(playerInput) {
 
     }
 
-    function displayResult(argComputerMove, argPlayerMove){
+    const displayResult = function(argComputerMove, argPlayerMove){
         if(argComputerMove === 'kamień' && argPlayerMove == 'papier'){
         printMessage('Ty wygrywasz!');
         } else if(argComputerMove === 'kamień' && argPlayerMove == 'nożyce'){
